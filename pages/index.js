ce38f620eeae7afe565b983ce5be47640213a7d9
Anderson Guerra <and.guerra@outlook.com>
2022-11-01 15:58:49 -0300

    Atualizações

--- pages/index.js
+++ pages/index.js
@@ -4,5 +4,24 @@ import Banner from "../public/imgs/cara3.png";
 import HomeStoreList from "../components/HomeStoreList";
 
 export default function Home() {
-  return <>Home</>;
+  return (
+    <>
+      <Row>
+        <Col
+          md={6}
+          style={{ borderRadius: "24px", backgroundColor: "#2596be" }}
+        >
+          <Row className="text-white align-items-center mt-3">
+            <Col>
+              <Image src={Banner} />
+            </Col>
+            <Col>
+              <h2>O MarketPlace</h2>
+              <h3>Venha vender com a gente :D</h3>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
 }
