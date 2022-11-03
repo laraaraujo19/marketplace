bf82426b240717b93bc77bf5c74f4f7e22150d57
Anderson Guerra <and.guerra@outlook.com>
2022-11-03 17:09:49 -0300

    Tela de Login e drawer implementados

--- pages/categorias/index.js
+++ pages/categorias/index.js
@@ -13,5 +13,26 @@ import {
 import * as Icon from "react-bootstrap-icons";
 
 export default function Categorias() {
-  return <>Categorias</>;
+  return (
+    <>
+      <Card className="m-md-5 p-md-5">
+        <h2 className="text-center">Categorias</h2>
+        <Row className="pt-2">
+          <Col md={10}>
+            <InputGroup>
+              <InputGroup.Text>
+                <Icon.Search />
+              </InputGroup.Text>
+              <Form.Control type="text" placeholder="Pesquisar..." />
+            </InputGroup>
+          </Col>
+          <Col className="d-grid">
+            <Link href="/categorias/cadastrar">
+              <Button>Adicionar</Button>
+            </Link>
+          </Col>
+        </Row>
+      </Card>
+    </>
+  );
 }
